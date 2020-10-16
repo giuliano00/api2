c2b2bfa08f0bea0629911e50867e5b642a758cd3
giuliano00 <giuliano.botto@davinci.edu.ar>
2020-10-16 14:13:38 -0300

    ok

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var request = require('request');
 
-request('https://kitsu.io/api/edge/anime', function (error, response, body) {
+request('https://kitsu.io/api/edge/episodes/28', function (error, response, body) {
   console.log('Status:', response.statusCode);
   console.log('Headers:', JSON.stringify(response.headers));
   console.log('Response:', body);
+  
 
   var objJson = JSON.parse(body);
-  console.log('Titulo: ', objJson.tittle);
+  console.log('Titulo: ', objJson.data.attributes.created.At);
+  //console.log('Titulo: ', objJson.data.attributes.titles.en);
   
 });
